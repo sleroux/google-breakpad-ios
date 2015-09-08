d46be11b1b760e1e8b47c1333f0b7be86150950e
Stephan Leroux <sleroux@mozilla.com>
2015-09-08 15:12:11 -0400

    Added in missing BreakpadiOS framework target

---
+++ src/client/ios/BreakpadiOS/BreakpadiOS.h
@@ -0,0 +1,21 @@
+//
+//  BreakpadiOS.h
+//  BreakpadiOS
+//
+//  Created by Steph Leroux on 2015-09-08.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BreakpadiOS.
+FOUNDATION_EXPORT double BreakpadiOSVersionNumber;
+
+//! Project version string for BreakpadiOS.
+FOUNDATION_EXPORT const unsigned char BreakpadiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BreakpadiOS/PublicHeader.h>
+#import <BreakpadiOS/Breakpad.h>
+#import <BreakpadiOS/BreakpadDefines.h>
+#import <BreakpadiOS/BreakpadController.h>
+
